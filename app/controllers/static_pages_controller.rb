ebf7fbe65a06d1b0df5fa42c84a94c9fb03d63c8
IPIvliev <cccp1945@mail.ru>
2015-10-01 17:17:29 +0300

    Add title for all pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def index
   	@articles = Article.where("view = ?", true).order("created_at DESC").limit(6)
 
-    set_meta_tags :title => "",
+    set_meta_tags :title => "Главная",
       :description => "Портал о строительстве и строительных коммуникациях. Сборник практических советов по самостоятельному строительству",
       :keywords => "строительство, строительные коммуникации, пластиковые окна, строй сам, водоотведение, водоснабжение дома, отопление частного дома, электричество в доме, тёплый пол в квартире"
   end
@@ -10,7 +10,7 @@ class StaticPagesController < ApplicationController
   def contacts
   	add_breadcrumb "Контакты", "/contacts.html"
 
-    set_meta_tags :title => "",
+    set_meta_tags :title => "Контакты",
       :description => "Связаться с администратором сайта",
       :keywords => "связь, строительство, реклама в строительстве"  	
   end
@@ -18,7 +18,7 @@ class StaticPagesController < ApplicationController
   def about
   	add_breadcrumb "О проекте", "/about.html"
 
-    set_meta_tags :title => "",
+    set_meta_tags :title => "О проекте",
       :description => "Информация о проекте Строй Сам",
       :keywords => "строительный портал, строительство, строительный сайт, информация о стройке, советы по стройке"
   end
